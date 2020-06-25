db9e7f53af5917981234cc8ebde4b274820b21fa
Luca Zampetti <lzampetti@gmail.com>
2020-06-25 11:23:54 +0200

    b-here-barilla

--- docs/js/main.js
+++ docs/js/main.js
@@ -553,7 +553,7 @@
       key: "href",
       get: function get() {
         if (window.location.host.indexOf('herokuapp') !== -1) {
-          return 'https://raw.githack.com/actarian/b-here/b-here-barilla/docs/';
+          return 'https://raw.githubusercontent.com/actarian/b-here/b-here-barilla/docs/';
         } else {
           return BASE_HREF;
         }
