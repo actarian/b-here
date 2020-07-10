d9e8b2afaf1fbf1ec20180c4a30062f59d663c79
Diego <30624958+diegoUE8@users.noreply.github.com>
2020-07-10 15:02:25 +0200

    herokuapp

--- src/js/environment.js
+++ src/js/environment.js
@@ -15,7 +15,7 @@ export class Environment {
 
 	get href() {
 		if (window.location.host.indexOf('herokuapp') !== -1) {
-			return 'https://raw.githubusercontent.com/actarian/b-here/b-here-barilla/docs/';
+			return 'https://raw.githubusercontent.com/actarian/b-here/b-here-ws/docs/';
 		} else {
 			return BASE_HREF;
 		}
