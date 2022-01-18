9105062260f8f4c972fc40643f09e50abbfeb424
Luca Zampetti <lzampetti@gmail.com>
2022-01-18 16:09:07 +0100

    modified: ModelMenuComponent, modal css, added: NavLinkClose

--- src/js/world/world.component.js
+++ src/js/world/world.component.js
@@ -1345,6 +1345,12 @@ export default class WorldComponent extends Component {
 						this.navLink.next(item);
 					}
 					break;
+				case MessageType.NavLinkClose:
+					const closeItem = this.view.items.find(item => item.id === message.itemId);
+					if (closeItem) {
+						ModalService.resolve();
+					}
+					break;
 				case MessageType.PlayMedia: {
 					// !!! uniformare a PlayModel
 					const item = this.view.items.find(item => item.id === message.itemId);
